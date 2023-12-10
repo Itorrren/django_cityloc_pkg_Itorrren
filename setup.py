971867f7ffc92c9de39314130736a3a5b5d4a010
Oleksandr <s24082004s@gmail.com>
2023-12-10 22:25:21 +0200

    publish package

--- setup.py
+++ setup.py
@@ -1,15 +1,9 @@
 from setuptools import setup, find_packages
-from pathlib import Path
 
 # put the contents of your README file into the long_description
+from pathlib import Path
 this_directory = Path(__file__).parent
-readme_path = this_directory / "README.rst"
-
-try:
-    long_description = readme_path.read_text()
-except FileNotFoundError:
-    print(f"File {readme_path} does not exist. Please ensure the file is in the correct location.")
-    long_description = ""
+long_description = (this_directory / "README.rst").read_text()
 
 setup(
     long_description=long_description
