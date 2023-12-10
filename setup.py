0380ec51a257f9263e272a12f9b987f53cfb59f2
Oleksandr <s24082004s@gmail.com>
2023-12-10 22:47:54 +0200

    try n2

--- setup.py
+++ setup.py
@@ -1,10 +1,25 @@
 from setuptools import setup, find_packages
-
-# put the contents of your README file into the long_description
 from pathlib import Path
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.rst").read_text()
+
+# Get the directory where this script is located
+this_directory = Path(__file__).resolve().parent
+
+# Use an absolute path to the README.rst file
+readme_path = this_directory / "README.rst"
+
+# Check if the README file exists
+if readme_path.is_file():
+    # Read the contents of the README file
+    long_description = readme_path.read_text()
+else:
+    # Provide a placeholder message if the README file is not found
+    long_description = "No README.rst file found."
 
 setup(
-    long_description=long_description
+    # ... other setup parameters ...
+
+    # Set the long_description using the contents of README.rst
+    long_description=long_description,
+    # Specify the content type for the long_description
+    long_description_content_type="text/x-rst",
 )
